5d3c2c1d744eb421fff0dedadb04b27d6b8f9e50
Pyunghyuk Yoo <yoophi@gmail.com>
2021-06-24 20:15:30 +0900

    🔧 INIT: prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
-  extends: ["eslint:recommended"],
+  extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 6,
   },
   env: {
     node: true,
+  },
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'comma-dangle': ['error', 'always-multiline'],
   },
 };

75dab596bb7393efdf78ffe8c0655a16136b9f95
Pyunghyuk Yoo <yoophi@gmail.com>
2021-06-24 20:07:18 +0900

    🚚 MOVE: tests into /tests

--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -1,4 +1,4 @@
-const palindromes = require("./palindromes");
+const palindromes = require("../palindromes");
 
 describe("palindromes()", () => {
   it("correctly identifies one-word palindromes", () => {
